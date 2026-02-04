67fa4f9eef7d37be25cf96053a03a17c747b759c
Ernests <ernestsgg@gmail.com>
2026-02-04 21:10:07 +0200

    Error fix for font preload

--- header.php
+++ header.php
@@ -6,10 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="preload" href="fonts/Montserrat-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" href="fonts/Montserrat-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" href="fonts/Montserrat-Medium.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" href="fonts/Montserrat-ExtraBold.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" href="/weatherapp/fonts/Montserrat-Regular.woff2" as="font" type="font/woff2"
+          crossorigin="anonymous">
+    <link rel="preload" href="/weatherapp/fonts/Montserrat-Bold.woff2" as="font" type="font/woff2"
+          crossorigin="anonymous">
+    <link rel="preload" href="/weatherapp/fonts/Montserrat-Medium.woff2" as="font" type="font/woff2"
+          crossorigin="anonymous">
+    <link rel="preload" href="/weatherapp/fonts/Montserrat-ExtraBold.woff2" as="font" type="font/woff2"
+          crossorigin="anonymous">
 
     <link rel="stylesheet" href="/weatherapp/css/compiled/style.css">
 
