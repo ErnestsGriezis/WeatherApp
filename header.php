573d5e6bb220a7dd3f7c2cdf0e799cd01eef80f6
Ernests <ernestsgg@gmail.com>
2026-02-09 17:27:03 +0200

    added favicon

--- header.php
+++ header.php
@@ -15,6 +15,12 @@
     <link rel="preload" href="/fonts/Montserrat-ExtraBold.woff2" as="font" type="font/woff2"
           crossorigin="anonymous">
 
+    <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+    <link rel="manifest" href="/images/favicon/site.webmanifest">
+
     <link rel="stylesheet" href="/css/compiled/style.css">
 
     <title>Weather App</title>
